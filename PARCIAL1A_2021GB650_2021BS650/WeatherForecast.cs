52e52bba9dc0e8db03248ada3518d68a66f4bc06
Esme Garcìa <esmeralda.garcia1@catolica.edu.sv>
2024-03-09 08:34:09 -0600

    Agregar archivos de proyecto.

---
+++ PARCIAL1A_2021GB650_2021BS650/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace PARCIAL1A_2021GB650_2021BS650
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
