29a9b7d8118b9b7d5552cd48e09bcc9ad544d0fa
Esme Garcìa <esmeralda.garcia1@catolica.edu.sv>
2024-03-09 08:41:26 -0600

    Context

--- PARCIAL1A_2021GB650_2021BS650/Models/parcial1aContext.cs
+++ PARCIAL1A_2021GB650_2021BS650/Models/parcial1aContext.cs
@@ -1,6 +1,11 @@
-﻿namespace PARCIAL1A_2021GB650_2021BS650.Models
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PARCIAL1A_2021GB650_2021BS650.Models
 {
-    public class parcial1aContext
+    public class parcial1aContext : DbContext
     {
+        public parcial1aContext(DbContextOptions options) : base(options)
+        {
+        }
     }
 }
