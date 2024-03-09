d1c81e4b420ed9a2f9b4b2e4a360e1c9d9e49c17
Diego Burgos <d.a.burgos.servellon@gmail.com>
2024-03-09 09:22:24 -0600

    CRUD Libros y AutorLibros

--- PARCIAL1A_2021GB650_2021BS650/Controllers/autorLibroController.cs
+++ PARCIAL1A_2021GB650_2021BS650/Controllers/autorLibroController.cs
@@ -2,14 +2,97 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using PARCIAL1A_2021GB650_2021BS650.Controllers;
+using PARCIAL1A_2021GB650_2021BS650.Models;
 
 namespace PARCIAL1A_2021GB650_2021BS650.Controllers
 {
     public class autorLibroController : Controller
     {
-        public IActionResult Index()
+
+        private readonly parcial1aContext _parcialContexto;
+
+        public autorLibroController(parcial1aContext parcial1aContexto)
         {
-            return View();
+            _parcialContexto = parcial1aContexto;
+        }
+
+        ///sumary
+        /// EndPoint que retorna el listado de todos los equipos existentes
+        /// summary
+        /// <return></return>
+        [HttpGet]
+        [Route("GetAllLibros")]
+        public IActionResult Get()
+        {
+            List<autorlibro> listadoAutorLibro = (from e in _parcialContexto.autorlibro
+                                                  select e).ToList();
+
+            if (listadoAutorLibro.Count == 0)
+            {
+                return NotFound();
+            }
+            return Ok(listadoAutorLibro);
+        }
+
+
+        [HttpPost]
+        [Route("Add")]
+
+        public IActionResult GuardarIdAutor([FromBody] autorlibro autorlibros)
+        {
+            try
+            {
+                _parcialContexto.autorlibro.Add(autorlibros);
+                _parcialContexto.SaveChanges();
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut]
+        [Route("actualizar/{id}")]
+
+        public IActionResult Actualizare(int id, [FromBody] autorlibro autorLibroModificar)
+        {
+            autorlibro? autorLibroActual = (from e in _parcialContexto.autorlibro
+                                     where e.AutorId == id
+                                     select e).FirstOrDefault();
+
+            if (autorLibroActual == null)
+            {
+                return NotFound();
+            }
+
+            autorLibroActual.AutorId = autorLibroModificar.AutorId;
+            autorLibroActual.LibroId = autorLibroModificar.LibroId;
+
+
+            _parcialContexto.Entry(autorLibroActual).State = EntityState.Modified;
+            _parcialContexto.SaveChanges();
+
+            return Ok(autorLibroModificar);
+
+        }
+
+        [HttpDelete]
+        [Route("eliminar/{id}")]
+        public IActionResult EliminarE(int id)
+        {
+            autorlibro? autoresLibros = (from e in _parcialContexto.autorlibro
+                               where e.AutorId == id
+                               select e).FirstOrDefault();
+
+            if (autoresLibros == null)
+                return NotFound();
+
+
+            _parcialContexto.autorlibro.Remove(autoresLibros);
+            _parcialContexto.SaveChanges();
+
+            return Ok(autoresLibros);
         }
     }
 }
--- PARCIAL1A_2021GB650_2021BS650/Controllers/librosController.cs
+++ PARCIAL1A_2021GB650_2021BS650/Controllers/librosController.cs
@@ -2,14 +2,112 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using PARCIAL1A_2021GB650_2021BS650.Controllers;
+using PARCIAL1A_2021GB650_2021BS650.Models;
 
 namespace PARCIAL1A_2021GB650_2021BS650.Controllers
 {
     public class librosController : Controller
     {
-        public IActionResult Index()
+        private readonly parcial1aContext _parcialContexto;
+
+        public librosController(parcial1aContext parcial1aContexto)
         {
-            return View();
+            _parcialContexto = parcial1aContexto;
+        }
+
+        ///sumary
+        /// EndPoint que retorna el listado de todos los equipos existentes
+        /// summary
+        /// <return></return>
+        [HttpGet]
+        [Route("GetAll")]
+        public IActionResult Get()
+        {
+            List<libros> listadoLibros = (from e in _parcialContexto.libros
+                                           select e).ToList();
+
+            if (listadoLibros.Count == 0)
+            {
+                return NotFound();
+            }
+            return Ok(listadoLibros);
+        }
+
+        // BUSCAR POR NOMBRE
+        [HttpGet]
+        [Route("GetLibroByNombre/{nombre}")]
+
+        public IActionResult Get(string nombre)
+        {
+            libros? libro = (from e in _parcialContexto.libros
+                               where e.Titulo == nombre
+                             select e).FirstOrDefault();
+            if (libro == null)
+            {
+                return NotFound();
+            }
+            return Ok(libro);
+        }
+
+
+        [HttpPost]
+        [Route("Add")]
+
+        public IActionResult Guardare([FromBody] libros libro)
+        {
+            try
+            {
+                _parcialContexto.libros.Add(libro);
+                _parcialContexto.SaveChanges();
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut]
+        [Route("actualizar/{id}")]
+
+        public IActionResult Actualizare(int id, [FromBody] libros libroModificar)
+        {
+            libros? libroActual = (from e in _parcialContexto.libros
+                                   where e.Id == id
+                                     select e).FirstOrDefault();
+
+            if (libroActual == null)
+            {
+                return NotFound();
+            }
+
+            libroActual.Id = libroModificar.Id;
+            libroActual.Titulo = libroModificar.Titulo;
+         
+            _parcialContexto.Entry(libroActual).State = EntityState.Modified;
+            _parcialContexto.SaveChanges();
+
+            return Ok(libroModificar);
+
+        }
+
+        [HttpDelete]
+        [Route("eliminar/{id}")]
+        public IActionResult EliminarE(int id)
+        {
+            libros? libro = (from e in _parcialContexto.libros
+                                where e.Id == id
+                               select e).FirstOrDefault();
+
+            if (libro == null)
+                return NotFound();
+
+            _parcialContexto.libros.Attach(libro);
+            _parcialContexto.libros.Remove(libro);
+            _parcialContexto.SaveChanges();
+
+            return Ok(libro);
         }
     }
 }
+
